10b4d22b557a586881c5fd63945e055c72405ca2
Sebastian Moreno <sdmorenob@unal.edu.co>
2019-09-30 01:23:24 -0500

    Init with basic sign up menu and connection with backend

--- src/store.js
+++ src/store.js
@@ -1,11 +1,11 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    backURL: 'http://localhost:8080'
   },
   mutations: {
 
